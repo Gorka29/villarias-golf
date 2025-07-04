94574f96405a5cc15a8681e2331b1536f0be1b64
GORKA RUIZ ARAUJO <gorka.ruiz.contractor@openpay.com.es>
2025-07-04 22:59:21 +0200

    Contenido escuela

--- src/app/escuela/escuela.component.ts
+++ src/app/escuela/escuela.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-escuela',
   standalone: true,
-  template: `<div class='flex flex-col items-center justify-center min-h-screen w-full bg-white'><h1 class='text-3xl font-bold mb-4'>Escuela</h1><p class='text-lg text-gray-700'>Página de información sobre la escuela de golf.</p></div>`
+  templateUrl: './escuela.component.html',
+  styleUrls: ['./escuela.component.scss']
 })
 export class EscuelaComponent {}
