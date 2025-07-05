8d8c29114ad7b97396ed884f6815e6f89ad43da0
GORKA RUIZ ARAUJO <gorka.ruiz.contractor@openpay.com.es>
2025-07-05 22:49:52 +0200

    Contenido de campo

--- src/app/campo/campo.component.ts
+++ src/app/campo/campo.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-campo',
   standalone: true,
-  template: `<div class='flex flex-col items-center justify-center min-h-screen w-full bg-white'><h1 class='text-3xl font-bold mb-4'>Campo</h1><p class='text-lg text-gray-700'>Página de información sobre el campo de golf.</p></div>`
+  templateUrl: './campo.component.html',
+  styleUrls: ['./campo.component.scss']
 })
 export class CampoComponent {}
