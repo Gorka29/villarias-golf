ff8b1abf0da1e3bfe9d254c8f1832f130a97d4b4
Gorka29 <gorkaruizaraujo@gmail.com>
2026-03-01 13:18:37 +0100

    Añadir pagina de tarifas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { MainContentComponent } from './main-content/main-content.component';
 export const routes: Routes = [
   { path: '', component: MainContentComponent, pathMatch: 'full' },
   { path: 'campo', loadComponent: () => import('./campo/campo.component').then(m => m.CampoComponent) },
+  { path: 'tarifas', loadComponent: () => import('./tarifas/tarifas.component').then(m => m.TarifasComponent) },
   { path: 'escuela', loadComponent: () => import('./escuela/escuela.component').then(m => m.EscuelaComponent) },
   { path: 'torneos', loadComponent: () => import('./torneos/torneos.component').then(m => m.TorneosComponent) },
   { path: 'torneos/masters-villarias', loadComponent: () => import('./torneos/masters-villarias-page/masters-villarias-page.component').then(m => m.MastersVillariasPageComponent) },
